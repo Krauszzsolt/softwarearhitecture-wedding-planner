3f0e25b23da08797762b235ebd2f09a01e5d14d7
unknown <horvathdes97@gmail.com>
2020-11-22 16:16:44 +0100

    backend init

---
+++ src/backend/BLL/Exceptions/RegistrationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BLL.Exceptions
+{
+    public class RegistrationException : Exception
+    {
+        public RegistrationException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/backend/BLL/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.DTOs.Authentication;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<ApplicationUserDto> AuthenticateAsync(LoginDto model);
+
+        Task<ApplicationUserDto> RegisterAsync(RegisterDto model);
+
+        Task<ApplicationUserDto> GetByIdAsync(string id);
+    }
+}
