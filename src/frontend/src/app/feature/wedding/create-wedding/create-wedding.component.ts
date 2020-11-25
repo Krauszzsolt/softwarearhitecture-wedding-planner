6e6d0d0a832e9453725f49757730f03eebcd055e
Krausz Zsolt <krausz.zsolt97@gmail.com>
2020-11-25 11:23:03 +0100

    wedding create scaffold

--- src/frontend/src/app/feature/wedding/create-wedding/create-wedding.component.ts
+++ src/frontend/src/app/feature/wedding/create-wedding/create-wedding.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-wedding',
@@ -6,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-wedding.component.scss'],
 })
 export class CreateWeddingComponent implements OnInit {
-  constructor() {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
-  ngOnInit() {}
+  wedding: FormGroup;
+  loading = false;
+  error = '';
+
+  url: string | ArrayBuffer = 'https://localhost:44329/images/book-cover-placeholder.png';
+
+  ngOnInit(): void {
+    this.wedding = this.formBuilder.group({
+      title: ['', Validators.required],
+      responsible: [''],
+      name: [''],
+    });
+  }
+
+  get f() {
+    return this.wedding.controls;
+  }
+
+  onFileChange(event) {}
+
+
+
 }
--- src/frontend/src/app/feature/wedding/wedding.module.ts
+++ src/frontend/src/app/feature/wedding/wedding.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { WeddingRoutingModule } from './wedding-routing.module';
 import { CreateWeddingComponent } from './create-wedding/create-wedding.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [CreateWeddingComponent],
-  imports: [CommonModule, WeddingRoutingModule],
+  imports: [CommonModule, WeddingRoutingModule, SharedModule],
 })
 export class WeddingModule {}
