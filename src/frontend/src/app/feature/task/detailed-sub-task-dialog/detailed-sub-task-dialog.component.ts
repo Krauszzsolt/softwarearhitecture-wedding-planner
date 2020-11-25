af2a00748e66e078a53f663ccabe6a0fc24cd8ba
Krausz Zsolt <krausz.zsolt97@gmail.com>
2020-11-25 15:26:27 +0100

    detaild sub task dialog

--- src/frontend/src/app/feature/task/detailed-sub-task-dialog/detailed-sub-task-dialog.component.ts
+++ src/frontend/src/app/feature/task/detailed-sub-task-dialog/detailed-sub-task-dialog.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { NewCommentDto, TaskDto } from 'src/app/shared/client';
+import { TaskManagementService } from '../service/task-management.service';
 
 @Component({
   selector: 'app-detailed-sub-task-dialog',
@@ -9,29 +12,56 @@ import { Router } from '@angular/router';
   styleUrls: ['./detailed-sub-task-dialog.component.scss'],
 })
 export class DetailedSubTaskDialogComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<DetailedSubTaskDialogComponent>, private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    public dialogRef: MatDialogRef<DetailedSubTaskDialogComponent>,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private taskManagementSerice: TaskManagementService,
+    private route: ActivatedRoute,
+    private userService: AuthService
+  ) {}
 
-  newStoryForm: FormGroup;
+  commentForm: FormGroup;
   loading = false;
   error = '';
-
+  taskDto: TaskDto;
   url: string | ArrayBuffer = 'https://localhost:44329/images/book-cover-placeholder.png';
-
+  newCommentDto: NewCommentDto;
   ngOnInit(): void {
-    this.newStoryForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      desreption: [''],
+    const id = (this.route.snapshot.paramMap.get('id') as undefined) as number;
+    this.taskManagementSerice.getTaskDetail(id).subscribe((resp) => {
+      this.taskDto = resp;
+    });
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.required],
     });
   }
 
   get f() {
-    return this.newStoryForm.controls;
+    return this.commentForm.controls;
   }
 
   onFileChange(event) {}
 
-
   onSubmit(): void {
- 
+    const id = (this.route.snapshot.paramMap.get('id') as undefined) as number;
+    this.newCommentDto = {
+      author: this.userService.currentUserValue.userName,
+      content: this.f.comment.value,
+    };
+    this.taskManagementSerice.addTaskComment(id, this.newCommentDto).subscribe(() => {});
+  }
+
+  delete() {
+    const id = (this.route.snapshot.paramMap.get('id') as undefined) as number;
+    this.taskManagementSerice.deleteTask(id).subscribe(() => {
+      this.dialogRef.close();
+    });
+  }
+  complete() {
+    const id = (this.route.snapshot.paramMap.get('id') as undefined) as number;
+    this.taskManagementSerice.completeTask(id).subscribe(() => {
+      this.dialogRef.close();
+    });
   }
 }
