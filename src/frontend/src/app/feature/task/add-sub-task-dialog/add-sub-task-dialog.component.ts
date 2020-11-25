b72c86c8cc15871c507229dee5e93ed61d3b79d2
Krausz Zsolt <krausz.zsolt97@gmail.com>
2020-11-25 14:40:30 +0100

    add sub task dialog

--- src/frontend/src/app/feature/task/add-sub-task-dialog/add-sub-task-dialog.component.ts
+++ src/frontend/src/app/feature/task/add-sub-task-dialog/add-sub-task-dialog.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NewTaskDto, NewTaskGroupDto, WeddingDto } from 'src/app/shared/client';
+import { TaskManagementService } from '../service/task-management.service';
 
 @Component({
   selector: 'app-add-sub-task-dialog',
@@ -9,56 +11,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-sub-task-dialog.component.scss'],
 })
 export class AddSubTaskDialogComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<AddSubTaskDialogComponent>, private formBuilder: FormBuilder, private router: Router) {}
+  constructor(public dialogRef: MatDialogRef<AddSubTaskDialogComponent>, private formBuilder: FormBuilder, private taskManagementSerice: TaskManagementService, private route: ActivatedRoute) {}
 
-  newStoryForm: FormGroup;
+  subTask: FormGroup;
   loading = false;
   error = '';
-
+  newTaskDto: NewTaskDto;
   url: string | ArrayBuffer = 'https://localhost:44329/images/book-cover-placeholder.png';
 
+  public wedding: WeddingDto;
   ngOnInit(): void {
-    this.newStoryForm = this.formBuilder.group({
+    this.taskManagementSerice.getTaskGroup().subscribe((x) => {
+      this.wedding = x;
+    });
+    this.subTask = this.formBuilder.group({
       title: ['', Validators.required],
       desreption: [''],
+      requiredTasks: [''],
     });
   }
 
   get f() {
-    return this.newStoryForm.controls;
-  }
-
-  onFileChange(event) {}
-
-  deselect(selected: string): void {
-    // const index: number = this.f.tags.value.indexOf(selected);
-    // if (index > -1) {
-    //   const newValue: string[] = this.f.tags.value.map((x) => x);
-    //   newValue.splice(index, 1);
-    //   this.f.tags.setValue(newValue);
-    // }
+    return this.subTask.controls;
   }
 
   onSubmit(): void {
     // stop here if form is invalid
-    if (this.newStoryForm.invalid) {
+    if (this.subTask.invalid) {
       return;
     }
 
-    let tags: string[] = [];
-    if (this.f.tags.value) {
-      tags = this.f.tags.value.map((x) => x);
-    }
+    this.newTaskDto = {
+      name: this.f.task.value,
+      responsible: this.f.requiredTasks.value,
+      description: this.f.desreption.value,
+      taskGroupId: this.route.snapshot.paramMap.get('id') as undefined as number
 
+    };
     this.loading = true;
-    // this.animationsService.animationsAddPost(this.f.title.value, this.f.coverImageSource.value).subscribe({
-    //   next: () => {
-    //     this.dialogRef.close();
-    //   },
-    //   error: (error) => {
-    //     this.error = error;
-    //     this.loading = false;
-    //   },
-    // });
+    this.taskManagementSerice.addTask(this.newTaskDto).subscribe({
+      next: () => {
+        this.dialogRef.close();
+      },
+      error: (error) => {
+        this.error = error;
+        this.loading = false;
+      },
+    });
   }
 }
