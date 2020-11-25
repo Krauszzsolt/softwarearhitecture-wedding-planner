6641d67c1a03e47e1fc352e5a42eed3efccc9c84
Krausz Zsolt <krausz.zsolt97@gmail.com>
2020-11-25 10:39:26 +0100

    reactive form guest

--- src/frontend/src/app/feature/guest/guest-invite/guest-invite.component.ts
+++ src/frontend/src/app/feature/guest/guest-invite/guest-invite.component.ts
@@ -1,35 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-guest-invite',
   templateUrl: './guest-invite.component.html',
-  styleUrls: ['./guest-invite.component.scss']
+  styleUrls: ['./guest-invite.component.scss'],
 })
 export class GuestInviteComponent implements OnInit {
-
-  public guestList: FormGroup;
   public loading = false;
   public error = '';
   constructor(public dialog: MatDialog, private router: Router, private formBuilder: FormBuilder) {}
+  public guestList = new FormArray([]);
 
   ngOnInit() {
-    this.guestList = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
+    this.addGuest();
+    this.addGuest();
+    this.addGuest();
+    this.addGuest();
+    this.f
+  }
+
+  addGuest() {
+    const group = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
     });
+
+    this.guestList.push(group);
   }
 
   get f() {
-    return this.guestList.controls;
+    console.log(this.guestList.controls)
+    console.log(this.guestList)
+     return this.guestList.controls as FormGroup[];
   }
 
+  delete(index){
+    this.guestList.removeAt(index);
+}
 
   mainTaskSelect() {
     console.log('jeeh');
     // this.router.navigateByUrl('task/1')
   }
-
 }
