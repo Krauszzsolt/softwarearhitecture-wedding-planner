b6d5116964a08ec3f4515b65360daa570724a5eb
Dénes Horváth <horvathdes97@gmail.com>
2020-11-27 14:41:07 +0100

    picture list

--- src/frontend/src/app/feature/picture/picture-list/picture-list.component.ts
+++ src/frontend/src/app/feature/picture/picture-list/picture-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WeddingService } from 'src/app/shared/client';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../../../core/service/auth.service';
 
 @Component({
   selector: 'app-picture-list',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PictureListComponent implements OnInit {
 
-  constructor() { }
+  pictures: string[] = null;
+  BASE_URL = environment.API_BASE_URL;
+
+  constructor(private weddingService: WeddingService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.weddingService.weddingIdPicturesGet(this.authService.currentUserValue.weddingId).subscribe(result => {
+      this.pictures = result;
+    });
   }
 
 }
