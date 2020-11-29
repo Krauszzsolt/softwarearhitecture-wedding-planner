979992d5780e8f09887a1d37c5a1f57d4157008e
Dénes Horváth <horvathdes97@gmail.com>
2020-11-29 09:36:43 +0100

    ensure DB is created

--- src/backend/API/Program.cs
+++ src/backend/API/Program.cs
@@ -1,5 +1,9 @@
+using DAL.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Backend
 {
@@ -7,7 +11,29 @@ namespace Backend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            EnsureDbCreated(host);
+
+            host.Run();
+        }
+
+        private static void EnsureDbCreated(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
